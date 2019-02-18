88176fd6504fa496ff07368dbe3d9f495a82ddf3
SirSaleh <animatorsaleh@gmail.com>
2019-02-18 17:35:44 +0330

    Implement histogram to Charts module

--- django_simple_charts/Charts.py
+++ django_simple_charts/Charts.py
@@ -1,4 +1,4 @@
-
+import math
 
 class Chart(object):
     """
@@ -91,7 +91,6 @@ class Chart(object):
             return None
 
 
-
 class QuantitativeChart(Chart):
 
     @staticmethod
@@ -126,3 +125,101 @@ class QuantitativeChart(Chart):
             if not self.max_x:
                 self.max_x = max(x_max_list)
 
+
+class Histogram(QuantitativeChart):
+    """
+    Histogram object, style, range and plot
+    """
+    queryset = None
+    breaks = None
+    bins_width = None
+    _histogram_dictionary = dict()
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def _get_quantile(data, r):
+        """
+        Get Quantile for r proportion (ie. 0.25 for Q1 and 0.5 for median)
+        :param data:
+        :param r:
+        :return:
+        """
+        if data:
+            size = data.__len__()
+            return data[math.floor(size * r)]
+
+    def _get_interquartile_range(self, data):
+        """
+        Get Interquantile Range (Q3 - Q1)
+        :param data:
+        :return:
+        """
+        if data:
+            return self._get_quantile(data, 0.75) - self._get_quantile(data, 0.25)
+
+    def _get_default_histogram_width(self, data):
+        """
+        Get Default histogram withs
+        :param data:
+        :return:
+        """
+        if data:
+            return 2 * self._get_interquartile_range(data) / (data.__len__() ** (1 / 3))
+
+    def _get_aggregated_data(self):
+        """
+        Aggregate all columns data in one list
+        :return:
+        """
+        if self.data_dictionary:
+            raw_list = [self._try_get_nested_value_from_dictionary(self.data_dictionary, x, 'data') for x in
+                        self.columns]
+            return [y for x in raw_list for y in x]
+
+    @staticmethod
+    def _count_data_in_range(data, min, max):
+        """
+        Count Number of data on a specific range for a list
+        :param data:
+        :param min:
+        :param max:
+        :return:
+        """
+        return [x for x in data if min <= x < max].__len__()
+
+    def _get_histogram_dictionary_for_column(self, column):
+        """
+        Get list of dictionaries (one list for each bin)
+        :param column:
+        :return:
+        """
+        histogram_column = list()
+        data = self._try_get_nested_value_from_dictionary(self.data_dictionary, column, 'data')
+        if data:
+            for bin_index in range(self.breaks):
+                min_bin = self.min_x + bin_index * self.bins_width
+                max_bin = min_bin + self.bins_width
+                histogram_column.append({'min': min_bin, 'max': max_bin,
+                                         'count': self._count_data_in_range(data, min_bin, max_bin)})
+
+            return histogram_column
+
+    def complete_histogram_attrs(self):
+        """
+        Complete histogram specific attributes
+        :return:
+        """
+        range_of_date = self.max_x - self.min_x
+
+        if not self.breaks:
+            aggregated_data = self._get_aggregated_data()
+            if self._get_default_histogram_width(aggregated_data):
+                self.bins_width = self._get_default_histogram_width(aggregated_data)
+                self.breaks = math.ceil(range_of_date / self.bins_width)
+        else:
+            self.bins_width = math.ceil(range_of_date / self.breaks)
+
+        for column in self.columns:
+            self._histogram_dictionary[column] = self._get_histogram_dictionary_for_column(column)
